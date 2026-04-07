6365d19ccca40e59dcc439f61ba7301340078407
GabrieldoCarmoooo <gabrieldocarmobezerra@gmail.com>
2026-04-06 21:17:36 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Vite + React + Tailwind + TypeScript 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
